bb67d5e52829b7461bfe0e732c2586437fa57140
ANKUR KUMAR <ankurkumar77019@gmail.com>
2025-01-06 20:40:49 +0530

    Enhance text-to-speech functionality
    Added the speak function using gTTS and pygame for MP3-based playback and audio control.

--- jarvis.py
+++ jarvis.py
@@ -17,3 +17,22 @@ newsapi = "<Your Key Here>"
 def speak_old(text):
     engine.say(text)
     engine.runAndWait()
+    def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
+
+    # Initialize Pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    pygame.mixer.music.load('temp.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3")
